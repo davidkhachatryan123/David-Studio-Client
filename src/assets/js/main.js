c61108098a12cf8050e52217f9b7e6e51fcde4aa
davidkhachatryan123 <xdavit777@icloud.com>
2022-11-29 00:20:13 +0400

    00:19 29-Nov-22

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -11,19 +11,17 @@ $(window).scroll(function() {
 });
 
 // jQuery to add padding for path
+// jQuery for page scrolling feature - requires jQuery Easing plugin
 $( document ).ready(function() {
-    if($('#path').length){
+    if($('#path').length) {
         $(".navbar-custom").addClass("path-padding");
     }
 });
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$(document).on('click', '#pagebutton', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
 });
